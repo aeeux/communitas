5364b9b3578d64304f192545e7c6a4d4a61fb2e2
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-06 22:50:16 +0100

    data bindings

--- app/views/home/home.component.ts
+++ app/views/home/home.component.ts
@@ -5,18 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  posts: any[];
+  goals: any[];
 
   //Firebase state variable for loggedInUser {{ isLoggedIn ? 'VIEW#1' : 'VIEW#2' }}
 
   constructor() {
-    this.posts = [
+    this.goals = [
       {
-        title: 'How Old is Old?',
-        intro:
-          'What we consider “old” changes dramatically with age, and our perspectives can be predictive of how well we age',
-        image: 'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
-        body: 'Our views of aging change significantly as we get older, influenced by many factors, including our current age and health status. In turn, what we deem as “old,” and how we see life playing out when we become old, can affect how quickly we get there. I’ll explain below. But first, some numbers: Millennials typically view 59 as old, while Boomers put the threshold at around 73, according to a U.S. survey that’s a little old itself (it was done in 2017). More recently, scientists interviewed 300 Canadians ranging in age from 45 to 100. On average, they pegged 73.7 years on the planet as the start of old age. People under 65 put it at 70.5, on average, and those over 65 set it at 77.4.',
+        genderEquality: {
+          title: 'Gender Equality',
+          intro: 'Some Intro about Gender Equality',
+          image:
+            'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
+          body: 'main text body of something about Gender Equality',
+        },
+        qualityEducation: {
+          title: 'Quality Education',
+          intro: 'Some Intro about Quality Education',
+          image:
+            'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
+          body: 'main text body of something about Quality Education',
+        },
       },
     ];
   }
