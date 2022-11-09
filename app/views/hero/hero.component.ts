39176a85adf1e8a366ed3661cc111b6c512daecb
outman1998 <outmanmounir98@gmail.com>
2022-11-09 10:10:27 +0100

    hero section oprettet og stylet
    hero section oprettet og stylet

--- app/views/hero/hero.component.ts
+++ app/views/hero/hero.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
   constructor() {}
