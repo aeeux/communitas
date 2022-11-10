7c212fd1818cb9fcde42b47addbcf0c65a363921
outman1998 <outmanmounir98@gmail.com>
2022-11-10 17:16:00 +0100

    styling til hero og content components
    styling til hero og content components

--- app/views/content/content.component.ts
+++ app/views/content/content.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
   constructor() {}
