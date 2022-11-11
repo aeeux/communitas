3caf28fd90bdf7f523635f7da68039043c51c7cc
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-11 18:58:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBlue: "#273137",
+      },
+    },
   },
   plugins: [],
 };
