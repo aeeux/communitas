291a743b576abc60c41f27039509f815bf4cc0eb
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-11 18:58:21 +0100

    Update intro.component.ts

--- app/views/intro/intro.component.ts
+++ app/views/intro/intro.component.ts
@@ -1,42 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.css']
+  styleUrls: ['./intro.component.css'],
 })
 export class IntroComponent implements OnInit {
-  goals: any[];
+  @Input() intro;
 
-  //Firebase state variable for loggedInUser {{ isLoggedIn ? 'VIEW#1' : 'VIEW#2' }}
-
-  constructor() {
-    this.goals = [
-      {
-        genderEquality: {
-          title: 'Gender Equality',
-          intro: 'Some Intro about Gender Equality',
-          image:
-            'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
-          body: 'main text body of something about Gender Equality',
-        },
-        qualityEducation: {
-          title: 'Quality Education',
-          intro: 'Some Intro about Quality Education',
-          image:
-            'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
-          body: 'main text body of something about Quality Education',
-        },
-        lifeBelowWater: {
-          title: 'Life Below Water',
-          intro: 'Some Intro about Life Below Water',
-          image:
-            'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
-          body: 'main text body of something about Life Below Water',
-        },
-      },
-    ];
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
