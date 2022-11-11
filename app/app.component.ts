b2127c1fd859762cff5fa6f93292466f34240ae4
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-11 13:37:07 +0100

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { loadingController } from '@ionic/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  genderEquality = {
+    title: '',
+    intro: '',
+    image: '',
+    body: '',
+  };
+
+  lifeBelowWater = {
+    title: '',
+    intro: '',
+    image: '',
+    body: '',
+  };
+
+  GenderEquality() {
+    this.genderEquality = {
+      title: 'Gender Equality',
+      intro: 'Some Intro about Gender Equality',
+      image: 'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
+      body: 'main text body of something about Gender Equality',
+    };
+    this.lifeBelowWater = {
+      title: '',
+      intro: '',
+      image: '',
+      body: '',
+    };
+  }
+
+  LifeBelowWater() {
+    this.genderEquality = {
+      title: '',
+      intro: '',
+      image: '',
+      body: '',
+    };
+    this.lifeBelowWater = {
+      title: 'Life Below Water',
+      intro: 'Some Intro about Life Below Water',
+      image: 'https://miro.medium.com/max/720/1*a5h7AJISKYMspgWVmU5pYA.jpeg',
+      body: 'main text body of something about Life Below Water',
+    };
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
