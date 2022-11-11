553418c13d6f40598ca36c138c1d04ef7f5ef5af
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-11 13:37:10 +0100

    Update content.component.ts

--- app/views/content/content.component.ts
+++ app/views/content/content.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
 })
 export class ContentComponent implements OnInit {
+  @Input() genderEquality;
+  @Input() lifeBelowWater;
+
+  //lifeBelowWater: Array<string>
   constructor() {}
 
   ngOnInit() {}
