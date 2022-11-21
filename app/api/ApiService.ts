730e8acb030e6ec2bfa850cbb1bd62201170c2fe
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-21 13:33:38 +0100

    Update ApiService.ts

--- app/api/ApiService.ts
+++ app/api/ApiService.ts
@@ -13,8 +13,17 @@ export class ApiService {
   apiUrl = 'http://localhost:3000/article';
 
   //get all article data
-
   getAllArticleData(): Observable<any> {
     return this._http.get(`${this.apiUrl}`);
   }
+
+  //Create
+  createData(data: any): Observable<any> {
+    return this._http.post(`${this.apiUrl}`, data);
+  }
+
+  //get article data for params /:id (1)
+  //get article data for params /:id (2)
+  //get article data for params /:id (3)
+  //get article data for params /:id (4)
 }
