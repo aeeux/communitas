1f3c780b3a0d626a6d6176b2b772f8880722e223
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-11-21 17:49:10 +0100

    admin comp

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-
+import { AdminComponent } from './views/admin/admin/admin.component';
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: '/admin',
+    component: AdminComponent,
   },
 ];
 
