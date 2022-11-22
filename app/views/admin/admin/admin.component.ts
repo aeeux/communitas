b219622c1839e5498469854b30b58a855ba41840
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-22 14:34:08 +0100

    Update admin.component.ts

--- app/views/admin/admin/admin.component.ts
+++ app/views/admin/admin/admin.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { ApiService } from 'app/api/ApiService';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -9,19 +11,26 @@ import {Router} from '@angular/router';
 export class AdminComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {}
+  readData: any;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private service: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: '',
-      password: ''
+      password: '',
     });
   }
 
   submit(): void {
-    this.http.post('http://http://localhost:8100/admin', this.form.getRawValue(), {
-    withCredentials: true})
-    .subscribe(() => this.router.navigate(['adminpage']));
+    this.service
+      .postAccountData(this.form.getRawValue(), {
+        withCredentials: true,
+      })
+      .subscribe(() => this.router.navigate(['adminpage']));
   }
-
 }
