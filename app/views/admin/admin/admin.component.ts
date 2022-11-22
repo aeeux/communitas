359214f0c29125f5cabccdcbe55d63f8990ae717
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-11-22 12:54:37 +0100

    Update admin.component.ts

--- app/views/admin/admin/admin.component.ts
+++ app/views/admin/admin/admin.component.ts
@@ -1,13 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
 })
 export class AdminComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://http://localhost:8100/admin', this.form.getRawValue(), {
+    withCredentials: true})
+    .subscribe(() => this.router.navigate(['adminpage']));
+  }
 
 }
