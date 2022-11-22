a353392bc21ecf3fde67ada86e4dd5f4972d81a4
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-22 15:02:01 +0100

    Update adminpage.component.ts

--- app/views/adminpage/adminpage.component.ts
+++ app/views/adminpage/adminpage.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Emitters } from 'app/emitters/emitters';
 
 @Component({
   selector: 'app-adminpage',
   templateUrl: './adminpage.component.html',
-
 })
 export class AdminpageComponent implements OnInit {
+  message = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
-
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:3000/accounts', { withCredentials: true })
+      .subscribe(
+        (res: any) => {
+          this.message = `Hi! ${res.data[0].username}`;
+          Emitters.authEmitter.emit(true);
+        },
+        (err) => {
+          this.message = 'You are not logged in';
+          Emitters.authEmitter.emit(false);
+        }
+      );
+  }
 }
