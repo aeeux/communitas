63b026b6ca9f7240b7ed0d9e2f9cd982236ebedb
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-11-22 12:54:14 +0100

    Update app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AdminComponent } from './views/admin/admin/admin.component';
+import { AdminpageComponent } from './views/adminpage/adminpage.component';
 const routes: Routes = [
   {
     path: '',
@@ -10,6 +11,10 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+  },
+  {
+    path: 'adminpage',
+    component: AdminpageComponent,
   },
 ];
 
