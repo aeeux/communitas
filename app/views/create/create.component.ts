b7f8dc94a7834fb3ae779bb114f73da44254eb33
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-11-26 01:10:49 +0100

    Update create.component.ts

--- app/views/create/create.component.ts
+++ app/views/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { ApiService } from 'app/api/ApiService';
 
@@ -9,23 +10,42 @@ import { ApiService } from 'app/api/ApiService';
 })
 export class CreateComponent implements OnInit {
   message = '';
+  readData: any;
+  errormsg: any;
+  getparamid: any;
+  successmsg: any;
 
-  constructor(public auth: AuthService, public service: ApiService) {}
+  constructor(
+    public auth: AuthService,
+    public service: ApiService,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.service.getAllArticleData().subscribe((res) => {
-      console.log(res, 'res ==>');
-
-      this.readData = res.data;
-    });
+    this.getAllData();
+    this.getparamid = this.router.snapshot.paramMap.get('id');
+    if (this.getparamid) {
+      this.service.getSingleArticleData(this.getparamid).subscribe((res) => {
+        this.articleForm.patchValue({
+          title: res.data[0].title,
+          descr: res.data[0].descr,
+          image: res.data[0].image,
+          prob1: res.data[0].prob1,
+          solu1: res.data[0].solu1,
+          prob2: res.data[0].prob2,
+          solu2: res.data[0].solu2,
+          prob3: res.data[0].prob3,
+          solu3: res.data[0].solu3,
+          prob4: res.data[0].prob4,
+          solu4: res.data[0].solu4,
+        });
+      });
+    }
   }
 
   logout() {
     this.auth.logout();
   }
-
-  readData: any;
-  errormsg: any;
 
   articleForm = new FormGroup({
     title: new FormControl('', Validators.required),
@@ -52,5 +72,25 @@ export class CreateComponent implements OnInit {
     } else {
       this.errormsg = 'all fields are required!';
     }
+  }
+
+  articleUpdate() {
+    if (this.articleForm.valid) {
+      this.service
+        .updateArticleData(this.articleForm.value, this.getparamid)
+        .subscribe((res) => {
+          this.successmsg = res.message;
+        });
+    } else {
+      this.errormsg = 'all fields are required!';
+    }
+  }
+
+  getAllData() {
+    this.service.getAllArticleData().subscribe((res) => {
+      console.log(res, 'res ==>');
+
+      this.readData = res.data;
+    });
   }
 }
